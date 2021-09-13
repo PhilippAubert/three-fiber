8fc81c9c9ccb7fceab17362d2dfb7d6881a0c877
philipp aubert <p.b.m.aubert@gmail.com>
2021-09-13 22:23:55 +0200

    Adjust canvas container to fill the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div id="canvas-container">
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
+          <mesh>
+            <boxGeometry args={[2, 2, 2]} />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
+      </div>
+    </div>
+  );
 }
 
 export default App;
