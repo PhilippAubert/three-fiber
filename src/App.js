9f77259bfeb920e9f1a4f92b5dd89cfd9cdb38b7
philipp aubert <p.b.m.aubert@gmail.com>
2021-09-14 12:34:07 +0200

    Add lights component and change to rectAreaLight

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Sphere from "./components/Sphere.js";
 import { Suspense } from "react";
+import Lights from "./components/Lights.js";
 
 function App() {
   return (
     <div className="App">
       <div id="canvas-container">
         <Canvas camera={{ position: [1, -2, 5], fov: 50 }}>
-          <ambientLight intensity={0.001} />
-          <directionalLight color="#f0f" position={[1, 1, 1]} />
+          <Lights brightness={10} color={"#f0f"} />
           <Suspense fallback={null}>
             <Sphere />
           </Suspense>
---
+++ src/components/Lights.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Lights({ brightness, color }) {
+  return (
+    <rectAreaLight
+      width={3}
+      height={3}
+      color={color}
+      intensity={brightness}
+      position={[-2, 0, 5]}
+      lookAt={[0, 0, 0]}
+      penumbra={1}
+      castShadow
+    />
+  );
+}
