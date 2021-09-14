212c8c8c0f9cac6f1db452ca0d6a5bf20280752c
philipp aubert <p.b.m.aubert@gmail.com>
2021-09-14 11:27:39 +0200

    Reinstall drei. Orbit controls for sphere available now

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import Sphere from "./components/Sphere.js";
+import { Suspense } from "react";
 
 function App() {
   return (
     <div className="App">
       <div id="canvas-container">
-        <Canvas>
+        <Canvas camera={{ position: [0, -4, 5], fov: 50 }}>
           <ambientLight intensity={0.001} />
           <directionalLight color="#f0f" position={[1, 1, 1]} />
-          <Sphere />
+          <Suspense>
+            <Sphere />
+          </Suspense>
+          <OrbitControls />
         </Canvas>
       </div>
     </div>
